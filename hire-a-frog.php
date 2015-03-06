c8d1e81a9282b8d3cada082a735a1460cd571b45
Alex Daniels <alex@knowmad.com>
2015-03-06 15:48:52 -0500

    edits to hire a frog page

--- hire-a-frog.php
+++ hire-a-frog.php
@@ -2,102 +2,29 @@
 
 <?php include 'inc/header.php';?>
 	
-		<div class="arrow-down sec1arrow"></div>
+	<div class="arrow-down sec1arrow"></div>
 
-		<div class="hero1">
-			<div class="herotext hidden">
-				<h1>Hire A FROG</h1>
-				<p>By setting the bar high for integrity and high performance in commercial construction, the FROG designation provides immediate recognition for companies of proven financial stability and quality so that they may be recognized by their worth.  All companies with the FROG Company designation are qualified through a strong set of criteria.</p>
-			</div>
-			<div class="buttonfrog">
-				<p><a class="scroll">Hire A FROG Company</a></p>
-			</div>
+	<div class="hero1">
+		<div class="herotext hidden">
+			<h1>Hire A FROG</h1>
+			<p>By setting the bar high for integrity and high performance in commercial construction, the FROG designation provides immediate recognition for companies of proven financial stability and quality so that they may be recognized by their worth.  All companies with the FROG Company designation are qualified through a strong set of criteria.</p>
 		</div>
-
-		<div class="page2">
-			<div class="center-titles padding-40">
-				<h1>$20/month per user to access FROG Companies</h2>
-				<h2>+ Enjoy all the benefits that come with being FROG Qualified!</h2>
-				<div class="center-button">
-					<p><a href="#">Learn More</a></p>
-				</div>
-			</div>
+		<div class="buttonfrog">
+			<p><a class="scroll">Hire A FROG Company</a></p>
 		</div>
-
-		<div class="arrow-down sec4arrow"></div>
-
-		<div class="four-column">
-
-			<h1>What does it take to be a FROG Company?</h1>
-			
-			<div class="hideme">
-				<a href="#">
-				<div class="column123">
-					<div class="frog">
-						<h1>F</h1>
-					</div>
-					<h2>Fair Business Practices</h2>
-					<ul>
-						<li>Fair</li>
-						<li>Reliable</li>
-						<li>Operational</li>
-						<li>Gifted</li>
-						<li class="click-me">Click to learn more</li>
-					</ul>
-				</div>
-				</a>
-
-				<a href="#">
-				<div class="column123">
-					<div class="frog">
-						<h1>R</h1>
-					</div>
-					<h2>Reliable Financial Standings</h2>
-					<ul>
-						<li>Fair</li>
-						<li>Reliable</li>
-						<li>Operational</li>
-						<li>Gifted</li>
-						<li class="click-me">Click to learn more</li>
-					</ul>
-				</div>
-				</a>
-
-				<a href="#">
-				<div class="column123">
-					<div class="frog">
-						<h1>O</h1>
-					</div>
-					<h2>Operational Excelence</h2>
-					<ul>
-						<li>Fair</li>
-						<li>Reliable</li>
-						<li>Operational</li>
-						<li>Gifted</li>
-						<li class="click-me">Click to learn more</li>
-					</ul>
-				</div>
-				</a>
-
-				<a href="#">
-				<div class="column4">
-					<div class="frog">
-						<h1>G</h1>
-					</div>
-					<h2>Gifted Expertise & Experience</h2>
-					<ul>
-						<li>Fair</li>
-						<li>Reliable</li>
-						<li>Operational</li>
-						<li>Gifted</li>
-						<li class="click-me">Click to learn more</li>
-					</ul>
-				</div>
-				</a>
-			</div>
-		</div>
-
 	</div>
+
+	<div class="page2">
+		<div class="center-titles padding-40">
+			<h1>$20/month per user to access FROG Companies</h2>
+			<h2>+ Enjoy all the benefits that come with being FROG Qualified!</h2>
+			<div class="center-button">
+				<p><a href="#">Learn More</a></p>
+			</div>
+		</div>
+	</div>
+
+	<div class="arrow-down sec4arrow"></div>
 
 	<div>
 		<div class="arrow-down sec1arrow top50"></div>
@@ -120,7 +47,7 @@
 						To apply through our website and submit online:
 					</p>
 					<div>
-						<a href="#"> Apply Online</a>
+						<a href="#"> Hire Online</a>
 					</div>
 				</div>
 			</div>
